916e67f5a633321434b1e758e40be0f6846b70a0
Ricardo Subtil <ricasubtil@gmail.com>
2023-04-24 12:05:27 +0100

    Sync API changes

--- utils/RegionUtils.gd
+++ utils/RegionUtils.gd
@@ -61,17 +61,19 @@ func globalize_date_dict(date_dict: Dictionary, source_format: String = ''):
 	return "%04d%02d%02dT%02d%02d%02d" % [year, month, day, hour, minute, second]
 
 func localize_age_rating(age_rating_raw: String) -> Control:
-	var rating_idx : int
-	match randi() % 3:
-		0:
-			rating_idx = 1
-		1:
-			rating_idx = 2
-		2, _:
-			rating_idx = 0
+	var rating_idx := localize_age_rating_idx()
 	var rating_node : Control = preload("res://addons/retrohub_theme_helper/ui/AgeRatingTextureRect.tscn").instance()
-	rating_node.from_rating_str(age_rating_raw, rating_idx)
+	rating_node.from_idx(int(age_rating_raw.get_slice("/", rating_idx)), rating_idx)
 	return rating_node
+
+func localize_age_rating_idx() -> int:
+	match RetroHubConfig.config.rating_system:
+		"pegi":
+			return 1
+		"cero":
+			return 2
+		"esrb", _:
+			return 0
 
 func localize_system_name(system_name: String) -> String:
 	if RetroHubConfig.config.system_names.has(system_name):
